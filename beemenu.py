6ca0ff8a43054bfbdfca42b1dc86c8e21b6b3f83
bzz-bee <67209327+bzz-bee@users.noreply.github.com>
2026-03-13 04:41:23 -0700

    Added appdict{}
    With app names as keys, their run commands as values

--- beemenu.py
+++ beemenu.py
@@ -5,7 +5,6 @@ from pathlib import Path
 import subprocess
 import sys
 
-
 class Window(QWidget):
     def __init__(self):
         super().__init__()
@@ -19,19 +18,16 @@ class Window(QWidget):
         layout.addWidget(applistW)
         self.setLayout(layout)
 
-        def onClick():
-            #for something?
-                print(command)
-                subprocess.run(command)
-
-        path1 = Path('/usr/share/applications')
-        path2 = Path('~/.local/share/applications')
-        #path1 = Path('test_dir/test1')
-        #path2 = Path('test_dir/test2')
+        #path1 = Path('/usr/share/applications')
+        #path2 = Path('~/.local/share/applications')
+        path1 = Path('test_dir/test1')
+        path2 = Path('test_dir/test2')
         filelist = []
         namelist = []
-        print(f'- Retrieving apps...')
+        commandlist = []
+        appdict = {}
 
+        print(f'- Retrieving apps & their run commands...')
         for file in path1.rglob('*.desktop'):
             filelist.append(file)
         for file in path2.rglob('*.desktop'):
@@ -47,7 +43,7 @@ class Window(QWidget):
                     nameline = line
                     noName = nameline.replace("Name=", "")
                     appname = noName.replace("\n", "")
-                    #Add it to the list
+                    #Add it to the name list
                     namelist.append(appname)
                     break
 
@@ -57,13 +53,29 @@ class Window(QWidget):
                     execline = line
                     noExec = execline.replace("Exec=", "")
                     command = noExec.replace("\n", "")
+                    #Add it to the command list
+                    commandlist.append(command)
                     break
+
+        for index in range(len(namelist)):
+            Name = namelist[index]
+            Command = commandlist[index]
+            appdict[Name] = Command
+        print(appdict)
 
         namelist.sort() #Alphabetical
         #Each app name in namelist becomes an item in QListWidget
+        #Must be after 'for index in range'
         applistW.addItems(namelist)
-        applistW.itemClicked.connect(onClick)
         print('- Apps added to app list.')
+
+        def onClick(item):
+            #prints the command for the last key...needs fix
+            valueCommand = appdict.get(item.text, Command)
+            print(valueCommand)
+            #subprocess.run(valueCommand)
+
+        applistW.itemClicked.connect(onClick)
         
         
         
